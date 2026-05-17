7af8da8baedc5cb23e2b37306d9e80dd2e18c72f
John Marzulli <john.marzulli@hotmail.com>
2026-05-16 22:24:47 -0700

    Hook up the fan target to the PWM on the output pin

--- InfernoEngine/FanHardwareController.h
+++ InfernoEngine/FanHardwareController.h
@@ -3,26 +3,31 @@
 
 class FanHardwareController {
 public:
-  FanHardwareController(
-    unsigned int ioPin) {
+  FanHardwareController(unsigned int ioPin) {
     _ioPin = ioPin;
+    pinMode(_ioPin, OUTPUT);
   }
 
-  void SetTarget(
-    float targetProportion) {
+  void SetTarget(float targetProportion) {
+    if (_target < 0.0f) {
+      _target = 0.0f;
+    } else if (_target > 1.0f) {
+      _target = 1.0f;
+    }
+
     _target = targetProportion;
+
+    printf("Setting target fan proportion: %f\n", _target);
   }
 
-  float GetTarget() {
-    return _target;
-  }
+  float GetTarget() { return _target; }
 
-  void Service(
-    float targetProportion) {
+  void Service(float targetProportion) {
+    _target = targetProportion;
+    analogWrite(_ioPin, (int)(_target * 255));
   }
 
 private:
-
   unsigned int _ioPin = 0;
   float _target = 0.0;
 };
