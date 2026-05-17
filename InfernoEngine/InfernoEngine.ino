ebb81458d21fe59d192ba04d142ce92a71eb2f0d
John Marzulli <john.marzulli@hotmail.com>
2026-05-16 22:25:54 -0700

    Use the PID or error controller based on connections.

--- InfernoEngine/InfernoEngine.ino
+++ InfernoEngine/InfernoEngine.ino
@@ -3,9 +3,12 @@
 #include "CombustionDeviceManager.h"
 #include "TemperatureReport.h"
 #include "ThermalControlManager.h"
+#include "UnconnectedControlManager.h"
 
+#define FAN_CONTROL_OUTPUT_PIN 0
 CombustionDeviceManager _combustionDeviceManager;
-FanHardwareController _fanHardwareController(0);
+FanHardwareController _fanHardwareController(FAN_CONTROL_OUTPUT_PIN);
+UnconnectedControlManager _unconnectedControlManger;
 ThermalControlManager _thermalControlManager;
 unsigned long _startTime;
 unsigned long _lastUpdateTime;
@@ -20,30 +23,39 @@ void setup() {
 
   _fanHardwareController.SetTarget(0.0);
 
+  _unconnectedControlManger = UnconnectedControlManager();
   _thermalControlManager = ThermalControlManager();
 }
 
 void loop() {
   unsigned long timeStamp = millis();
   unsigned long timeBetweenUpdates = 1000;
-
   // put your main code here, to run repeatedly:
   unsigned long timeSinceLastUpdate = timeStamp - _lastUpdateTime;
 
   _combustionDeviceManager.Service();
 
-  if (timeSinceLastUpdate > timeBetweenUpdates && _combustionDeviceManager.IsConnected()) {
+  bool isConnected = _combustionDeviceManager.IsConnected();
+
+  if (timeSinceLastUpdate > timeBetweenUpdates && isConnected) {
     _lastUpdateTime = millis();
 
     float currentPitTemperature = _combustionDeviceManager.GetCurrentTemperature();
     TemperatureReport newReport = TemperatureReport(timeStamp, currentPitTemperature);
 
     _thermalControlManager.AddReport(newReport);
+    printf("Added report. Temperature: %fC\n", currentPitTemperature);
   }
 
-  _thermalControlManager.Service();
+  float targetFanProportion = 0;
 
-  float targetFanProportion = _thermalControlManager.GetTargetFanProportion();
+  if (isConnected) {
+    _thermalControlManager.Service();
+    targetFanProportion = _thermalControlManager.GetTargetFanProportion();
+  } else {
+    _unconnectedControlManger.Service();
+    targetFanProportion = _unconnectedControlManger.GetTargetFanProportion();
+  }
 
   _fanHardwareController.Service(targetFanProportion);
 }
